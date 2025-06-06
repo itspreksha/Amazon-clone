46475ed086e60a57b8880f4d0adf2b6e89af696a
Preksha <prekshajoshi44@gmail.com>
2025-06-06 15:28:39 +0530

    Add URL routing and query parameter handling

--- Amazon/settings.py
+++ Amazon/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Amazon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'amazondb',
+        'USER': 'postgres',
+        'PASSWORD': '110400',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- Amazonclone/models.py
+++ Amazonclone/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    Category_choices= [
+        ('electronics' , 'Electronics'),
+        ('fashion','Fashion'),
+        ('books','Books'),
+        ('appliances','Appliances'),
+
+    ]
+    name = models.CharField(max_length=100)
+    price=models.FloatField()
+    description=models.TextField()
+    category=models.CharField(max_length=100,choices=Category_choices)
+    image=models.ImageField(upload_to='images/')
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
