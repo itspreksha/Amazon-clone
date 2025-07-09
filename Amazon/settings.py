19976b66658a6fa63dce42c01e40ccd8f2a18958
Preksha Joshi <156881662+itspreksha@users.noreply.github.com>
2025-07-09 16:24:50 +0530

    Update settings.py

--- Amazon/settings.py
+++ Amazon/settings.py
@@ -131,7 +131,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    }
+}
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
