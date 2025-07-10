633c171038dd8b7ea3d5668bd0aa380d5070e2e4
Preksha Joshi <156881662+itspreksha@users.noreply.github.com>
2025-07-10 15:04:25 +0530

    Update settings.py

--- Amazon/settings.py
+++ Amazon/settings.py
@@ -123,14 +123,13 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",  
+    os.path.join(BASE_DIR, 'Amazonclone/static'),  # change path based on your actual location
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
